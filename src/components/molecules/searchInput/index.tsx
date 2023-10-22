d365e1a08dc3250805f6c15e4e4b0e665089ba7f
zeromountain <sonyeongsan@gmail.com>
2023-10-22 22:31:56 +0900

    add query string for search word

--- src/components/molecules/searchInput/index.tsx
+++ src/components/molecules/searchInput/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import style from "./style.module.css";
 import { useDispatch } from "react-redux";
 import { setSearch } from "../../../store/searchSlice";
+import { useSearchParams } from "react-router-dom";
 
 export default function SearchInput() {
-  const [input, setInput] = React.useState("");
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [input, setInput] = React.useState(searchParams.get("q") || "");
   const dispatch = useDispatch();
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -12,32 +14,47 @@ export default function SearchInput() {
     setInput(e.target.value);
   };
 
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleSearch();
+  };
+
   const handleSearch = () => {
+    if (input.trim() === "") {
+      searchParams.delete("q");
+      setSearchParams(searchParams);
+      dispatch(setSearch(""));
+      return;
+    }
     if (isNaN(Number(input))) {
       alert("숫자만 입력해주세요");
       return;
+    } else {
+      searchParams.set("q", input);
+      setSearchParams(searchParams);
+      dispatch(setSearch(input));
     }
-    alert(input);
-    dispatch(setSearch(input));
   };
 
   return (
     <div className={`${style.container}`}>
-      <div className={`${style.relative}`}>
-        <input
-          className={`${style.input}`}
-          type="text"
-          placeholder="포켓몬 번호를 검색해보세요"
-          value={input}
-          onChange={onChangeSearch}
-        />
-        <img
-          className={`${style.icon}`}
-          src="/pokeball.png"
-          alt="search-icon"
-          onClick={handleSearch}
-        />
-      </div>
+      <form onSubmit={onSubmit}>
+        <div className={`${style.relative}`}>
+          <input
+            className={`${style.input}`}
+            type="text"
+            placeholder="포켓몬 번호를 검색해보세요"
+            value={input}
+            onChange={onChangeSearch}
+          />
+          <img
+            className={`${style.icon}`}
+            src="/pokeball.png"
+            alt="search-icon"
+            onClick={handleSearch}
+          />
+        </div>
+      </form>
     </div>
   );
 }
